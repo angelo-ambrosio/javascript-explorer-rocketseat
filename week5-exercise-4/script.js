d9d9b3f23394ad78b44c8781ea2acefe15a165e4
Angelo Ambrósio <angelotosta1105@gmail.com>
2023-03-22 12:09:03 -0300

    exercício 4 da semana 5 em andamento

---
+++ week5-exercise-4/script.js
@@ -0,0 +1,59 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+
+let minutes;
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+function countdown(){
+    setTimeout(function(){
+        let seconds = Number(secondsDisplay.textContent)
+
+        if(seconds <= 0){
+            seconds = 60
+        }
+
+        secondsDisplay.textContent = seconds - 1
+        countdown()
+    }, 1000)
+}
+
+buttonPlay.addEventListener('click', () => {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+    
+    countdown()
+})
+
+buttonPause.addEventListener('click', () => {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', () => {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+    buttonStop.classList.add('hide')
+    buttonSet.classList.remove('hide')
+})
+
+buttonSoundOn.addEventListener('click', () => {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+
+buttonSoundOff.addEventListener('click', () => {
+    buttonSoundOff.classList.add('hide')
+    buttonSoundOn.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', () => {
+    minutes = prompt("Quantos minutos?")
+    minutesDisplay.textContent = minutes
+})
